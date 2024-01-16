d6aa70a58abcbad68cd181cc343ba689e245aee0
juanrene2900 <e1104120595.uisrael.edu.ec>
2024-01-15 21:55:54 -0500

    Front y Backend establecido

---
+++ src/main/java/com/finesec/pictogramas/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.finesec.pictogramas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/principal") //url
+	public String inicio() { //metodo de ejecucion al leer la url
+		return "index"; //ruta fisica de la pagina web
+	}
+}
