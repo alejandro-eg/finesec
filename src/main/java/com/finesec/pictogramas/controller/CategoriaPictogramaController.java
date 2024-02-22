0cb78dfde4858e6147a966a6feff35dae1f52c12
juanrene2900 <e1104120595.uisrael.edu.ec>
2024-02-22 08:47:46 -0500

    Ultimos cambios 2

--- src/main/java/com/finesec/pictogramas/controller/CategoriaPictogramaController.java
+++ src/main/java/com/finesec/pictogramas/controller/CategoriaPictogramaController.java
@@ -18,45 +18,67 @@ public class CategoriaPictogramaController {
 	@Autowired
 	private ICategoriaPictogramaService servicioCategoriaPictograma;
 	private CategoriaPictograma nuevaCategoriaPictograma;
+	private Boolean editMode = false;
+
 	@GetMapping("/categoriapictograma")
 	public String listarCategoriaPictograma(Model model) {
-		List<CategoriaPictograma> datosCategoriaPictogramasDB= servicioCategoriaPictograma.ListarCategoriaPigtograma();
+		List<CategoriaPictograma> datosCategoriaPictogramasDB = servicioCategoriaPictograma.ListarCategoriaPigtograma();
 		model.addAttribute("ListaCP", datosCategoriaPictogramasDB);
-		 return "/categoriapictogramas/listarcategoriapictograma";
-		
+		return "/categoriapictogramas/listarcategoriapictograma";
+
 	}
+
 	@GetMapping("/nuevacategoriapictograma")
 	public String insertarCategoriaPictograma(Model model) {
 		nuevaCategoriaPictograma = new CategoriaPictograma();
 		model.addAttribute("nuevo", nuevaCategoriaPictograma);
 		return "/categoriapictogramas/nuevacategoriapictograma";
 	}
-	
+
 	@PostMapping("/guardarcategoriapictograma")
-	public String guardarCategoriaPictograma(@ModelAttribute("nuevo")CategoriaPictograma nuevaCategoriaPictograma, Model model){
-		CategoriaPictograma nombreExistente = servicioCategoriaPictograma.findByNombre(nuevaCategoriaPictograma.getNombre());
-		CategoriaPictograma keywordExistente = servicioCategoriaPictograma.findByNombreIngles(nuevaCategoriaPictograma.getNombreIngles());
+	public String guardarCategoriaPictograma(@ModelAttribute("nuevo") CategoriaPictograma nuevaCategoriaPictograma,
+			Model model) {
+		CategoriaPictograma nombreExistente = servicioCategoriaPictograma
+				.findByNombre(nuevaCategoriaPictograma.getNombre());
+		CategoriaPictograma keywordExistente = servicioCategoriaPictograma
+				.findByNombreIngles(nuevaCategoriaPictograma.getNombreIngles());
 		if (nombreExistente != null) {
-            // si la categoria existe mostrara una alerta
-            model.addAttribute("alerta", "La categoria ya existe. Por favor, elige otro nombre.");
-            return "/categoriapictogramas/nuevacategoriapictograma";
-        }else if (keywordExistente != null) {
-        	 model.addAttribute("alerta2", "La categoria ya existe. Por favor, elige otro keyword.");
-             return "/categoriapictogramas/nuevacategoriapictograma";
+
+			if (!editMode) {
+				// si la categoria existe mostrara una alerta
+				model.addAttribute("alerta", "La categoria ya existe. Por favor, elige otro nombre.");
+				editMode = false;
+				model.addAttribute("editMode", editMode);
+				return "/categoriapictogramas/nuevacategoriapictograma";
+			}
+		} else if (keywordExistente != null) {
+			if (!editMode) {
+				model.addAttribute("alerta2", "La categoria ya existe. Por favor, elige otro keyword.");
+				editMode = false;
+				model.addAttribute("editMode", editMode);
+				return "/categoriapictogramas/nuevacategoriapictograma";
+			}
+
 		}
 		servicioCategoriaPictograma.insertarCategoriaPictograma(nuevaCategoriaPictograma);
+		// Reinicia editMode
+	    editMode = false;
+	    model.addAttribute("editMode", editMode);
 		return "redirect:/categoriapictograma";
 	}
-	
+
 	@GetMapping("/editarcategoriapictograma/{idCategoriaPictograma}")
-	public String editarCategoriaPictograma(@PathVariable("idCategoriaPictograma") int idCategoriaPictograma, Model model) {
-	    CategoriaPictograma recuperadoDB = servicioCategoriaPictograma.findByIdCategoriaPictograma(idCategoriaPictograma);
-	    model.addAttribute("nuevo", recuperadoDB);
-	    return "/categoriapictogramas/nuevacategoriapictograma";
+	public String editarCategoriaPictograma(@PathVariable("idCategoriaPictograma") int idCategoriaPictograma,
+			Model model) {
+		CategoriaPictograma recuperadoDB = servicioCategoriaPictograma
+				.findByIdCategoriaPictograma(idCategoriaPictograma);
+		model.addAttribute("nuevo", recuperadoDB);
+		return "/categoriapictogramas/nuevacategoriapictograma";
 	}
-	
+
 	@GetMapping("/eliminarcategoriapictograma/{idCategoriaPictograma}")
-	public String eliminarCategoriaPictograma(@PathVariable(value="idCategoriaPictograma")int idCategoriaPictograma) {
+	public String eliminarCategoriaPictograma(
+			@PathVariable(value = "idCategoriaPictograma") int idCategoriaPictograma) {
 		servicioCategoriaPictograma.eliminarCategoriaPictograma(idCategoriaPictograma);
 		return "redirect:/categoriapictograma";
 	}
